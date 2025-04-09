81138a73b39a63177621cdea47adf6f1dfe3bdcd
Frederico Simas Duarte <fredmdfk@gmail.com>
2025-04-09 02:48:51 +0200

    Inject repository on HomeViewModel and add observable flows

--- app/src/main/java/com/sw/fred/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/sw/fred/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,34 @@
 package com.sw.fred.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sw.fred.data.domain.Category
+import com.sw.fred.data.repository.MealDbRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-): ViewModel() {}
+    private val repository: MealDbRepository,
+): ViewModel() {
+
+    private var _categories: MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
+    val category = _categories.asStateFlow()
+
+    /**
+     * Fetches the list of categories from the repository and emits it to the UI.
+     */
+    fun getCategories() {
+        viewModelScope.launch {
+            try {
+                _categories.emit(repository.getCategories())
+            } catch (e: Exception) {
+                Log.e("HomeViewModel", e.toString())
+            }
+        }
+    }
+}
