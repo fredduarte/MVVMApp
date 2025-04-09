eb5710af88acd39e86aac5bafb59ee19884a3341
Frederico Simas Duarte <fredmdfk@gmail.com>
2025-04-09 04:23:50 +0200

    On Home, Fetch all categories and their recipes

--- app/src/main/java/com/sw/fred/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/sw/fred/ui/viewmodel/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sw.fred.data.domain.Category
+import com.sw.fred.data.domain.Recipe
 import com.sw.fred.data.repository.MealDbRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,19 +17,59 @@ class HomeViewModel @Inject constructor(
     private val repository: MealDbRepository,
 ): ViewModel() {
 
-    private var _categories: MutableStateFlow<List<Category>> = MutableStateFlow(emptyList())
-    val category = _categories.asStateFlow()
+    private var _viewState: MutableStateFlow<ViewState> = MutableStateFlow(ViewState.Loading)
+    val viewState = _viewState.asStateFlow()
 
-    /**
-     * Fetches the list of categories from the repository and emits it to the UI.
-     */
-    fun getCategories() {
+    init {
+        getRecipesForAllCategories()
+    }
+
+    private fun getRecipesForAllCategories() {
+        _viewState.value = ViewState.Loading
         viewModelScope.launch {
             try {
-                _categories.emit(repository.getCategories())
+                // Fetch all categories
+                val categories = repository.getCategories()
+
+                // Fetch recipes for ALL categories
+                categories.forEach { category ->
+                    val recipes = repository.getRecipesByCategory(category = category.name)
+                    val allCategoryRecipes = recipes.map { recipe ->
+                        CategoryRecipes(category = category, recipes = recipes)
+                    }
+                    _viewState.value = ViewState.Success(
+                        recipes = allCategoryRecipes,
+                    )
+                }
             } catch (e: Exception) {
-                Log.e("HomeViewModel", e.toString())
+                Log.e(TAG, e.toString())
+                _viewState.value = ViewState.Error(
+                    message = e.message,
+                )
             }
         }
+    }
+
+    sealed interface ViewState {
+        data class Success(
+            val recipes: List<CategoryRecipes>,
+        ) : ViewState
+
+        data class Error(
+            val message: String?,
+        ) : ViewState
+
+        object Loading : ViewState
+
+        object NoResults : ViewState
+    }
+
+    data class CategoryRecipes(
+        val category: Category,
+        val recipes: List<Recipe>,
+    )
+
+    companion object {
+        private const val TAG = "HomeViewModel"
     }
 }
